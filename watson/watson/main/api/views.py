4d128b6d23e1024b8aefd2aa30fec7c11ce13824
Zhanara <lesovazhanara@gmail.com>
2019-08-24 18:01:22 +0600

    removed unnecessary staff

--- watson/watson/main/api/views.py
+++ watson/watson/main/api/views.py
@@ -10,7 +10,7 @@ class InsurancePackageViewset(viewsets.ModelViewSet):
     queryset = InsurancePackage.objects.all()
     serializer_class = InsurancePackageSerializer
 
-    @action(detail=True, methods='post')
+    @action(detail=True, methods=['post'])
     def create_package(self, requests):
         body = requests.data
         package_name = body.get('package_name')
@@ -19,7 +19,7 @@ class InsurancePackageViewset(viewsets.ModelViewSet):
         for category in Category.objects.filter(pk__in=category_list).distinct():
             InsurancePackageCategories.objects.create(category=category, package=package)
 
-    @action(detail=False, methods=["get"])
+    @action(detail=False)
     def base(self, request):
         base_packages = self.queryset.filter(is_base=True)
         return Response(self.serializer_class(base_packages, many=True).data)
@@ -28,3 +28,5 @@ class InsurancePackageViewset(viewsets.ModelViewSet):
 class CategoryViewset(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+
+
