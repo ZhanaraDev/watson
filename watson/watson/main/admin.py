e26e60e10e6f29246cb7434835d97a937aaa5f8a
Zhanara <lesovazhanara@gmail.com>
2019-08-24 17:03:55 +0600

    made api retrieving base packages with categories

--- watson/watson/main/admin.py
+++ watson/watson/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from main.models import Category, InsurancePackage, InsurancePackageCategories
+
+admin.site.register(Category)
+admin.site.register(InsurancePackage)
+admin.site.register(InsurancePackageCategories)
