1cfe1ea2f808e4a883ceb3b34255b48a35f1b58c
Zhanara <lesovazhanara@gmail.com>
2019-08-24 17:47:41 +0600

    cors headers and new viewset

--- watson/watson/main/api/urls.py
+++ watson/watson/main/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import InsurancePackageViewset
+from .views import InsurancePackageViewset, CategoryViewset
 
 
 router = DefaultRouter()
 router.register(r'insurance_package', InsurancePackageViewset)
+router.register(r'category', CategoryViewset)
 
 urlpatterns = router.urls
--- watson/watson/main/api/views.py
+++ watson/watson/main/api/views.py
@@ -2,19 +2,29 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from main.api.serializers import InsurancePackageSerializer
-from main.models import InsurancePackage
+from main.api.serializers import InsurancePackageSerializer, CategorySerializer
+from main.models import InsurancePackage, Category, InsurancePackageCategories
 
 
-class InsurancePackageViewset(viewsets.ViewSet):
+class InsurancePackageViewset(viewsets.ModelViewSet):
     queryset = InsurancePackage.objects.all()
     serializer_class = InsurancePackageSerializer
+
+    @action(detail=True, methods='post')
+    def create_package(self, requests):
+        body = requests.data
+        package_name = body.get('package_name')
+        category_list = body.get('category_list')
+        package = InsurancePackage.objects.create(package_name=package_name)
+        for category in Category.objects.filter(pk__in=category_list).distinct():
+            InsurancePackageCategories.objects.create(category=category, package=package)
 
     @action(detail=False, methods=["get"])
     def base(self, request):
         base_packages = self.queryset.filter(is_base=True)
-
         return Response(self.serializer_class(base_packages, many=True).data)
 
 
-
+class CategoryViewset(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
