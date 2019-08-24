ae460d481c61eae7a11f7c2cd18247b90b070e39
Zhanara <lesovazhanara@gmail.com>
2019-08-24 15:06:56 +0600

    ff

--- watson/watson/main/models.py
+++ watson/watson/main/models.py
@@ -3,33 +3,34 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    title = models.CharField(max_legth=250)
+    title = models.CharField(max_length=250)
     description = models.TextField()
 
 
 class ClientCompany(models.Model):
     name = models.CharField(max_length=250)
-    money_paid = models.DecimalField()
+    money_paid = models.DecimalField(max_digits=8, decimal_places=2)
 
 
 class InsurancePackage(models.Model):
     package_name = models.CharField(max_length=250)
+    is_base = models.BooleanField(default=False)
 
 
 class InsurancePackageCategories(models.Model):
-    category = models.ForeignKey(Category)
-    package = models.ForeignKey(InsurancePackage)
-    money_limit = models.DecimalField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    package = models.ForeignKey(InsurancePackage, on_delete=models.CASCADE)
+    money_limit = models.DecimalField(max_digits=8, decimal_places=2)
 
 
 class ClientCompanyEmployees(models.Model):
-    user = models.ForeignKey(User)
-    company = models.ForeignKey(ClientCompany)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    company = models.ForeignKey(ClientCompany, on_delete=models.CASCADE)
     name = models.CharField(max_length=250)
     surname = models.CharField(max_length=250)
     iin = models.CharField(max_length=250)
-    balance = models.DecimalField()
-    insurance_package = models.OneToOneField(InsurancePackage, null=True)
+    balance = models.DecimalField(max_digits=8, decimal_places=2)
+    insurance_package = models.OneToOneField(InsurancePackage, null=True, on_delete=models.CASCADE)
 
 
 class ServiceProvidingHolding(models.Model):
@@ -37,9 +38,9 @@ class ServiceProvidingHolding(models.Model):
 
 
 class ServiceProvider(models.Model):
-    name = models.CharField()
-    hospital = models.ForeignKey(ServiceProvidingHolding)
-    visit_cost = models.DecimalField()
+    name = models.CharField(max_length=250)
+    service_providing_holding = models.ForeignKey(ServiceProvidingHolding, on_delete=models.CASCADE)
+    visit_cost = models.DecimalField(max_digits=8, decimal_places=2)
 
 
 DAY_CHOICES = (
@@ -54,15 +55,15 @@ DAY_CHOICES = (
 
 
 class ServiceProviderSchedule(models.Model):
-    service_provider = models.ForeignKey(ServiceProvider)
-    day = models.CharField(choices=DAY_CHOICES)
+    service_provider = models.ForeignKey(ServiceProvider, on_delete=models.CASCADE)
+    day = models.CharField(choices=DAY_CHOICES, max_length=50)
     time_start = models.DateField()
     time_end = models.DateField()
 
 
 class Appointment(models.Model):
-    employee = models.ForeignKey(ClientCompany)
-    service_provider = models.ForeignKey(ServiceProvider)
+    employee = models.ForeignKey(ClientCompany, on_delete=models.CASCADE)
+    service_provider = models.ForeignKey(ServiceProvider, on_delete=models.CASCADE)
     date = models.DateField()
     time = models.TimeField()
-    week_day = models.CharField(choices=DAY_CHOICES)
+    week_day = models.CharField(choices=DAY_CHOICES, max_length=50)
